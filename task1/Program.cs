3fbc16a77376a72603f8ac3400b446d0dda5c460
Roman Chermyshov <romarioriw2005@gmail.com>
2023-05-22 20:39:37 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int n = 40;
+        int a = 0;
+        int b = 1;
+        Console.Write("Перші {0} чисел Фібаначі", n);
+        Console.Write(a + ", ");
+        for (var i = 0; i < n; i++)
+        {
+            Console.Write(b + ", ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        Console.Read();
+    }
+}
